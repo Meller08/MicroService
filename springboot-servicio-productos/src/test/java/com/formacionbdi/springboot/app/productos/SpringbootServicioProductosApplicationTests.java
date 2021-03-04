07db48539adfccf88113bf260c9b87ff70fc7a00
elmergarcia <elmerjg08@gmail.com>
2021-03-04 16:35:48 -0600

    package productos

--- springboot-servicio-productos/src/test/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplicationTests.java
+++ springboot-servicio-productos/src/test/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplicationTests.java
@@ -1,16 +1,13 @@
 package com.formacionbdi.springboot.app.productos;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootServicioProductosApplicationTests {
+class SpringbootServicioProductosApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
